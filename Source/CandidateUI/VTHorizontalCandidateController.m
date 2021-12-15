8b849cbf05b5597597e135e0479fe275c47add4a
ShikiSuen <shikisuen@outlook.com>
2021-12-15 19:11:02 +0800

    更新橫向選字窗格的美術風格
    Revert "更新橫向選字窗格的美術風格"
    
    This reverts commit 79afa5447740be1c5c0ee78775232a09258cb79d.

--- Source/CandidateUI/VTHorizontalCandidateController.m
+++ Source/CandidateUI/VTHorizontalCandidateController.m
@@ -53,7 +53,9 @@
     NSPanel *panel = [[NSPanel alloc] initWithContentRect:contentRect styleMask:styleMask backing:NSBackingStoreBuffered defer:NO];
     [panel setLevel:kCGPopUpMenuWindowLevel];
     [panel setHasShadow:YES];
-    
+    [panel setOpaque:NO];
+    [panel setBackgroundColor:[NSColor colorWithCalibratedWhite:1.0 alpha:0.0]];
+
     self = [self initWithWindow:panel];
     if (self) {
         contentRect.origin = NSMakePoint(0.0, 0.0);
@@ -62,20 +64,30 @@
         _candidateView.action = @selector(candidateViewMouseDidClick:);
         [[panel contentView] addSubview:_candidateView];
 
-        contentRect.size = NSMakeSize(36.0, 20.0);
+        contentRect.size = NSMakeSize(16.0, 20.0);
         _nextPageButton = [[NSButton alloc] initWithFrame:contentRect];
         _prevPageButton = [[NSButton alloc] initWithFrame:contentRect];
         [_nextPageButton setButtonType:NSMomentaryLightButton];
-        [_nextPageButton setBezelStyle:NSSmallSquareBezelStyle];
-        [_nextPageButton setTitle:@"»"];
+        [_nextPageButton setBezelStyle:NSBezelStyleSmallSquare];
+        [_nextPageButton setTitle:@"↓"];
         [_nextPageButton setTarget:self];
         [_nextPageButton setAction:@selector(pageButtonAction:)];
-
+        [_nextPageButton setWantsLayer: YES];
+        [_nextPageButton.layer setCornerRadius: 3];
+        [_nextPageButton.layer setBorderColor: [NSColor clearColor].CGColor];
+        [_nextPageButton.layer setBorderWidth: 3];
+        [_nextPageButton.layer setBackgroundColor: [NSColor windowBackgroundColor].CGColor];
+         
         [_prevPageButton setButtonType:NSMomentaryLightButton];
-        [_prevPageButton setBezelStyle:NSSmallSquareBezelStyle];
-        [_prevPageButton setTitle:@"«"];
+        [_prevPageButton setBezelStyle:NSBezelStyleSmallSquare];
+        [_prevPageButton setTitle:@"↑"];
         [_prevPageButton setTarget:self];
         [_prevPageButton setAction:@selector(pageButtonAction:)];
+        [_prevPageButton setWantsLayer: YES];
+        [_prevPageButton.layer setCornerRadius: 3];
+        [_prevPageButton.layer setBorderColor: [NSColor clearColor].CGColor];
+        [_prevPageButton.layer setBorderWidth: 3];
+        [_prevPageButton.layer setBackgroundColor: [NSColor windowBackgroundColor].CGColor];
         
         [[panel contentView] addSubview:_nextPageButton];
         [[panel contentView] addSubview:_prevPageButton];
