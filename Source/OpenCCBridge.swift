646dea12320a88653326504106e8d8052ab7f520
ShikiSuen <shikisuen@outlook.com>
2022-01-15 13:45:43 +0800

    Shiki: Swiftify // OpenCC Bridge Format Tweak

--- Source/OpenCCBridge.swift
+++ Source/OpenCCBridge.swift
@@ -1,3 +1,36 @@
+//
+// PreferencesWindowController.swift
+//
+// Copyright (c) 2011-2022 The OpenVanilla Project.
+//
+// Contributors:
+//     Weizhong Yang (@zonble) @ OpenVanilla
+//
+// Based on the Syrup Project and the Formosana Library
+// by Lukhnos Liu (@lukhnos).
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+
 import Foundation
 import OpenCC
 
@@ -7,16 +40,16 @@ class OpenCCBridge : NSObject {
     private static let shared = OpenCCBridge()
     private var converter: ChineseConverter?
 
-    override init() {
+    private override init() {
         try? converter = ChineseConverter(options: .twStandardRev)
         super.init()
     }
 
-    @objc static func convert(_ string:String) -> String? {
-        return shared.converter?.convert(string)
+    @objc static func convert(_ string: String) -> String? {
+        shared.converter?.convert(string)
     }
 
-    private func convert(_ string:String) -> String? {
-        return converter?.convert(string)
+    private func convert(_ string: String) -> String? {
+        converter?.convert(string)
     }
 }
