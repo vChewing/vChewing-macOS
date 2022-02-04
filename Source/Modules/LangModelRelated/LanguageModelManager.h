dd2f2f75205f9a4acf897283347325b048c6a3b1
ShikiSuen <shikisuen@outlook.com>
2022-02-04 21:44:49 +0800

    IME // Reorganizing project folder structure.
    - The color contrast of the menu icon for Simplified Chinese mode has been tweaked for better legibility.
    - Chinese localized name changed for both CHS and CHT input modes.
    - Changed the way how Xcode manages the app versions.
    - Renamed certain Window NIBs and their controllers to enforce Hungary naming convention.

---
+++ Source/Modules/LangModelRelated/LanguageModelManager.h
@@ -0,0 +1,39 @@
+/* 
+ *  LanguageModelManager.h
+ *  
+ *  Copyright 2021-2022 vChewing Project (3-Clause BSD License).
+ *  Derived from 2011-2022 OpenVanilla Project (MIT License).
+ *  Some rights reserved. See "LICENSE.TXT" for details.
+ */
+
+#import <Foundation/Foundation.h>
+#import "UserOverrideModel.h"
+#import "vChewingLM.h"
+#import "SSZipArchive.h"
+#import "AWFileHash.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LanguageModelManager : NSObject
+
++ (void)loadDataModels;
++ (void)deployZipDataFile:(NSString *)filenameWithoutExtension;
++ (void)loadCNSData;
++ (BOOL)checkIfCNSDataExistAndHashMatched;
++ (void)loadUserPhrases;
++ (void)loadUserPhraseReplacement;
++ (BOOL)checkIfUserLanguageModelFilesExist;
++ (BOOL)writeUserPhrase:(NSString *)userPhrase inputMode:(NSString *)inputMode;
++ (NSString *)userPhrasesDataPath:(NSString *)inputMode;
++ (NSString *)excludedPhrasesDataPath:(NSString *)inputMode;
++ (NSString *)phraseReplacementDataPath:(NSString *)inputMode;
++ (NSString *)cnsDataPath;
+
+@property (class, readonly, nonatomic) NSString *dataFolderPath;
+@property (class, readonly, nonatomic) vChewing::vChewingLM *languageModelCoreCHT;
+@property (class, readonly, nonatomic) vChewing::vChewingLM *languageModelCoreCHS;
+@property (class, readonly, nonatomic) vChewing::UserOverrideModel *userOverrideModelCHT;
+@property (class, readonly, nonatomic) vChewing::UserOverrideModel *userOverrideModelCHS;
+@end
+
+NS_ASSUME_NONNULL_END
