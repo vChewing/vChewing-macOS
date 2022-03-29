6b43145852b753f336f4fa20622cdba9f2b91023
ShikiSuen <shikisuen@outlook.com>
2022-03-29 10:35:30 +0800

    AppDelegate // Move LM initialization to IME module.

--- Source/Modules/IME.swift
+++ Source/Modules/IME.swift
@@ -31,6 +31,18 @@ import Cocoa
         }
     }
 
+    @objc static func initLangModels(userOnly: Bool) {
+        if !userOnly {
+            mgrLangModel.loadDataModels() // 這句還是不要砍了。
+        }
+        // mgrLangModel 的 loadUserPhrases 等函數在自動讀取 dataFolderPath 時，
+        // 如果發現自訂目錄不可用，則會自動抹去自訂目錄設定、改採預設目錄。
+        // 所以這裡不需要特別處理。
+        mgrLangModel.loadUserPhrases()
+        mgrLangModel.loadUserPhraseReplacement()
+        mgrLangModel.loadUserAssociatedPhrases()
+    }
+
     @objc static func isDarkMode() -> Bool {
         if #available(macOS 10.15, *) {
             let appearanceDescription = NSApplication.shared.effectiveAppearance.debugDescription.lowercased()
