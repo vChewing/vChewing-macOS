55cc8659ee37595a9afccb8823c0e3687b2192f9
ShikiSuen <shikisuen@outlook.com>
2022-03-31 14:50:55 +0800

    PKG // Upgrade post-installation script to fix wrong installations.

--- Installer/pkgPostInstall.sh
+++ Installer/pkgPostInstall.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-~/Library/Input\ Methods/vChewing.app/Contents/MacOS/vChewing install --all
+loggedInUser=$(stat -f%Su /dev/console)
+
+# First, copy the wrongfully installed contents to the right location:
+cp -r /Library/Input\ Methods/vChewing.app /Users/$(stat -f%Su /dev/console)/Library/Input\ Methods/ || true
+cp -r /Library/Keyboard\ Layouts/vChewing* /Users/$(stat -f%Su /dev/console)/Library/Keyboard\ Layouts/ || true
+chown "$loggedInUser" /Users/$(stat -f%Su /dev/console)/Library/Input\ Methods/vChewing.app || true
+chown "$loggedInUser" /Users/$(stat -f%Su /dev/console)/Library/Keyboard\ Layouts/vChewing* || true
+
+sleep 1
+
+# Second, clean the wrongfully installed contents:
+rm -rf /Library/Input\ Methods/vChewing.app || true
+rm -rf /Library/Keyboard\ Layouts/vChewing* || true
+sleep 1
+
+# Finally, register the input method:
+/Users/$(stat -f%Su /dev/console)/Library/Input\ Methods/vChewing.app/Contents/MacOS/vChewing install --all || true
