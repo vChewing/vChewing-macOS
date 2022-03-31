56896870030220d232c4fc3dc255ee5013086496
ShikiSuen <shikisuen@outlook.com>
2022-03-31 14:50:55 +0800

    PKG // Upgrade post-installation script to fix wrong installations.

--- Installer/pkgPreInstall.sh
+++ Installer/pkgPreInstall.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-killall vChewing
+loggedInUser=$(stat -f%Su /dev/console)
+
+killall vChewing || true
+
+if [ "$loggedInUser" = root ]; then
+    rm -rf /Library/Input\ Methods/vChewing.app || true
+    rm -rf /Library/Keyboard\ Layouts/vChewingKeyLayout.bundle || true
+    rm -rf /Library/Keyboard\ Layouts/vChewing\ Dachen.keylayout || true
+    rm -rf /Library/Keyboard\ Layouts/vChewing\ ETen.keylayout || true
+    rm -rf /Library/Keyboard\ Layouts/vChewing\ FakeSeigyou.keylayout || true
+    rm -rf /Library/Keyboard\ Layouts/vChewing\ IBM.keylayout || true
+    rm -rf /Library/Keyboard\ Layouts/vChewing\ MiTAC.keylayout || true
+fi
+
 rm -rf ~/Library/Input\ Methods/vChewing.app
 rm -rf ~/Library/Keyboard\ Layouts/vChewingKeyLayout.bundle
 rm -rf ~/Library/Keyboard\ Layouts/vChewing\ Dachen.keylayout
