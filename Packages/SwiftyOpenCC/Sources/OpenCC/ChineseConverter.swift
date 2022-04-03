5fba88cfeb4f63d3bf9efc59d8e0f6893c4ca86a
ShikiSuen <shikisuen@outlook.com>
2022-04-03 12:50:51 +0800

    OpenCC // Swift Clang-Format.

--- Packages/SwiftyOpenCC/Sources/OpenCC/ChineseConverter.swift
+++ Packages/SwiftyOpenCC/Sources/OpenCC/ChineseConverter.swift
@@ -22,68 +22,68 @@ import copencc
 /// However, the string on which it is operating should not be mutated
 /// during the course of a conversion.
 public class ChineseConverter {
-    
-    /// These constants define the ChineseConverter options.
-    public struct Options: OptionSet {
-        
-        public let rawValue: Int
-        
-        public init(rawValue: Int) {
-            self.rawValue = rawValue
-        }
-        
-        /// Convert to Traditional Chinese. (default)
-        public static let traditionalize = Options(rawValue: 1 << 0)
-        
-        /// Convert to Simplified Chinese.
-        public static let simplify = Options(rawValue: 1 << 1)
-        
-        /// Use Taiwan standard.
-        public static let twStandard = Options(rawValue: 1 << 5)
-        
-        /// Use HongKong standard.
-        public static let hkStandard = Options(rawValue: 1 << 6)
-        
-        /// Cancel Taiwan standard.
-        public static let twStandardRev = Options(rawValue: 1 << 15)
-        
-        /// Cancel HongKong standard.
-        public static let hkStandardRev = Options(rawValue: 1 << 16)
 
-        /// Taiwanese idiom conversion.
-        public static let twIdiom = Options(rawValue: 1 << 10)
-    }
-    
-    private let seg: ConversionDictionary
-    private let chain: [ConversionDictionary]
-    
-    private let converter: CCConverterRef
-    
-    private init(loader: DictionaryLoader, options: Options) throws {
-        seg = try loader.segmentation(options: options)
-        chain = try loader.conversionChain(options: options)
-        var rawChain = chain.map { $0.dict }
-        converter = CCConverterCreate("SwiftyOpenCC", seg.dict, &rawChain, rawChain.count)
-    }
-    
-    /// Returns an initialized `ChineseConverter` instance with the specified
-    /// conversion options.
-    ///
-    /// - Parameter options: The convert’s options.
-    /// - Throws: Throws `ConversionError` if failed.
-    public convenience init(options: Options) throws {
-        let loader = DictionaryLoader(bundle: .module)
-        try self.init(loader: loader, options: options)
-    }
-    
-    /// Return a converted string using the convert’s current option.
-    ///
-    /// - Parameter text: The string to convert.
-    /// - Returns: A converted string using the convert’s current option.
-    public func convert(_ text: String) -> String {
-        let stlStr = CCConverterCreateConvertedStringFromString(converter, text)!
-        defer { STLStringDestroy(stlStr) }
-        return String(utf8String: STLStringGetUTF8String(stlStr))!
-    }
-    
+	/// These constants define the ChineseConverter options.
+	public struct Options: OptionSet {
+
+		public let rawValue: Int
+
+		public init(rawValue: Int) {
+			self.rawValue = rawValue
+		}
+
+		/// Convert to Traditional Chinese. (default)
+		public static let traditionalize = Options(rawValue: 1 << 0)
+
+		/// Convert to Simplified Chinese.
+		public static let simplify = Options(rawValue: 1 << 1)
+
+		/// Use Taiwan standard.
+		public static let twStandard = Options(rawValue: 1 << 5)
+
+		/// Use HongKong standard.
+		public static let hkStandard = Options(rawValue: 1 << 6)
+
+		/// Cancel Taiwan standard.
+		public static let twStandardRev = Options(rawValue: 1 << 15)
+
+		/// Cancel HongKong standard.
+		public static let hkStandardRev = Options(rawValue: 1 << 16)
+
+		/// Taiwanese idiom conversion.
+		public static let twIdiom = Options(rawValue: 1 << 10)
+	}
+
+	private let seg: ConversionDictionary
+	private let chain: [ConversionDictionary]
+
+	private let converter: CCConverterRef
+
+	private init(loader: DictionaryLoader, options: Options) throws {
+		seg = try loader.segmentation(options: options)
+		chain = try loader.conversionChain(options: options)
+		var rawChain = chain.map { $0.dict }
+		converter = CCConverterCreate("SwiftyOpenCC", seg.dict, &rawChain, rawChain.count)
+	}
+
+	/// Returns an initialized `ChineseConverter` instance with the specified
+	/// conversion options.
+	///
+	/// - Parameter options: The convert’s options.
+	/// - Throws: Throws `ConversionError` if failed.
+	public convenience init(options: Options) throws {
+		let loader = DictionaryLoader(bundle: .module)
+		try self.init(loader: loader, options: options)
+	}
+
+	/// Return a converted string using the convert’s current option.
+	///
+	/// - Parameter text: The string to convert.
+	/// - Returns: A converted string using the convert’s current option.
+	public func convert(_ text: String) -> String {
+		let stlStr = CCConverterCreateConvertedStringFromString(converter, text)!
+		defer { STLStringDestroy(stlStr) }
+		return String(utf8String: STLStringGetUTF8String(stlStr))!
+	}
+
 }
