625cc1fe8fb4bc659ffc83d10b236e1603039941
ShikiSuen <shikisuen@outlook.com>
2022-05-01 10:33:02 +0800

    UPE // Comment cleanup.

--- UserPhraseEditor/WindowController.swift
+++ UserPhraseEditor/WindowController.swift
@@ -31,9 +31,6 @@ class WindowController: NSWindowController, NSWindowDelegate {
 
 	required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
-		/** NSWindows loaded from the storyboard will be cascaded
-         based on the original frame of the window in the storyboard.
-         */
 		shouldCascadeWindows = true
 	}
 }
