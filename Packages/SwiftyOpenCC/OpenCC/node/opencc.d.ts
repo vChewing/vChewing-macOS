6e21a1c9b99774be8c4451a28a03ae9581d82da7
ShikiSuen <shikisuen@outlook.com>
2022-07-03 19:09:36 +0800

    Repo // Deprecating OpenCC, the last C++ component in vChewing.

--- Packages/SwiftyOpenCC/OpenCC/node/opencc.d.ts
+++
@@ -1,9 +0,0 @@
-declare class OpenCC {
-  constructor(config: string);
-  version(): string;
-  generateDict(inputFileName: string, outputFileName: string, formatFrom: string, formatTo: string): void;
-  convert(input: string, callback: (err: string, convertedText: string) => void): string;
-  convertSync(input: string): string;
-  convertPromise(input: string): Promise<string>;
-}
-export { OpenCC };
