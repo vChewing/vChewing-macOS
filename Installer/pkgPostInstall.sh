3cb99bad70d44fa3ff1a37f32e2212a11c96960b
ShikiSuen <shikisuen@outlook.com>
2022-09-23 00:26:29 +0800

    PKG // Re-enable installation folder fix for macOS 12 and later.
    - We have received numerous cases that the pkg installs vChewing IME to the wrong place since 2.6.2 release.

--- Installer/pkgPostInstall.sh
+++ Installer/pkgPostInstall.sh
@@ -4,7 +4,7 @@ TARGET='vChewing'
 login_user=$(/usr/bin/stat -f%Su /dev/console)
 
 OS_Version=$(sw_vers -productVersion)
-if [[ ${OS_Version} < 12.0.0 ]]; then
+##### if [[ ${OS_Version} < 12.0.0 ]]; then
   # Copy the wrongfully installed contents to the right location:
   cp -r /Library/Input\ Methods/"${TARGET}".app /Users/"${login_user}"/Library/Input\ Methods/ || true
   cp -r /Library/Keyboard\ Layouts/"${TARGET}"* /Users/"${login_user}"/Library/Keyboard\ Layouts/ || true
@@ -16,7 +16,7 @@ if [[ ${OS_Version} < 12.0.0 ]]; then
   rm -rf /Library/Input\ Methods/"${TARGET}".app || true
   rm -rf /Library/Keyboard\ Layouts/"${TARGET}"* || true
   sleep 1
-fi
+##### fi
 
 # Finally, register the input method:
 /Users/"${login_user}"/Library/Input\ Methods/"${TARGET}".app/Contents/MacOS/"${TARGET}" install --all || true
