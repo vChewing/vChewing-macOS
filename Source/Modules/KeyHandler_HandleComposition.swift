6f59ba6a8a53bc813f7f274d682bc4e035bcb17f
ShikiSuen <shikisuen@outlook.com>
2022-10-02 15:18:31 +0800

    KeyHandler // Update certain comments.

--- Source/Modules/KeyHandler_HandleComposition.swift
+++ Source/Modules/KeyHandler_HandleComposition.swift
@@ -62,7 +62,7 @@ extension KeyHandler {
       composer.receiveKey(fromString: input.text)
       keyConsumedByReading = true
 
-      // 沒有調號的話，只需要 updateClientdisplayedText() 且終止處理（return true）即可。
+      // 沒有調號的話，只需要 setInlineDisplayWithCursor() 且終止處理（return true）即可。
       // 有調號的話，則不需要這樣，而是轉而繼續在此之後的處理。
       if !composer.hasToneMarker() {
         stateCallback(buildInputtingState)
@@ -119,7 +119,7 @@ extension KeyHandler {
       // 之後就是更新組字區了。先清空注拼槽的內容。
       composer.clear()
 
-      // 再以回呼組字狀態的方式來執行 updateClientdisplayedText()。
+      // 再以回呼組字狀態的方式來執行 setInlineDisplayWithCursor()。
       var inputting = buildInputtingState
       inputting.textToCommit = textToCommit
       stateCallback(inputting)
@@ -151,7 +151,7 @@ extension KeyHandler {
 
     /// 是說此時注拼槽並非為空、卻還沒組音。這種情況下只可能是「注拼槽內只有聲調」。
     if keyConsumedByReading {
-      // 以回呼組字狀態的方式來執行 updateClientdisplayedText()。
+      // 以回呼組字狀態的方式來執行 setInlineDisplayWithCursor()。
       stateCallback(buildInputtingState)
       return true
     }
