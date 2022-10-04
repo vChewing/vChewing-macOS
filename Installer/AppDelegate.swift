e3c1a89afb11d7bd91d93d366d492b52ff68e0c3
ShikiSuen <shikisuen@outlook.com>
2022-10-04 18:03:00 +0800

    AppInstaller // Advise re-login right after first-time installation.

--- Installer/AppDelegate.swift
+++ Installer/AppDelegate.swift
@@ -333,7 +333,7 @@ class AppDelegate: NSWindowController, NSApplicationDelegate {
           "Installation Successful", comment: ""
         )
         ntfPostInstall.informativeText = NSLocalizedString(
-          "vChewing is ready to use.", comment: ""
+          "vChewing is ready to use. \n\nPlease relogin if this is the first time you install it in this user account.", comment: ""
         )
         ntfPostInstall.addButton(withTitle: NSLocalizedString("OK", comment: ""))
       }
