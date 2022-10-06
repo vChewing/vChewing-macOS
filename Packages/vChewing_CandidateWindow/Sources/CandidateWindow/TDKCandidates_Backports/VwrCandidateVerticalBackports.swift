a47a10022596c844c336c8dc472f055fed791c0d
ShikiSuen <shikisuen@outlook.com>
2022-10-06 21:35:57 +0800

    TDKCandidates // Refactor for simplicity.

--- Packages/vChewing_CandidateWindow/Sources/CandidateWindow/TDKCandidates_Backports/VwrCandidateVerticalBackports.swift
+++ Packages/vChewing_CandidateWindow/Sources/CandidateWindow/TDKCandidates_Backports/VwrCandidateVerticalBackports.swift
@@ -6,7 +6,6 @@
 // marks, or product names of Contributor, except as required to fulfill notice
 // requirements defined in MIT License.
 
-import CandidateWindow
 import Cocoa
 import Shared
 import SwiftUI
@@ -15,30 +14,29 @@ import SwiftUIBackports
 // MARK: - Some useless tests
 
 @available(macOS 10.15, *)
-struct CandidatePoolViewUIHorizontalBackports_Previews: PreviewProvider {
+struct CandidatePoolViewUIVerticalBackports_Previews: PreviewProvider {
   @State static var testCandidates: [String] = [
-    "八月中秋山林涼", "八月中秋", "風吹大地", "山林涼", "草枝擺", "八月", "中秋",
-    "🐂🍺🐂🍺", "🐃🍺", "🐂🍺", "🐃🐂🍺🍺", "🐂🍺", "🐃🍺", "🐂🍺", "🐃🍺", "🐂🍺", "🐃🍺",
+    "八月中秋山林涼", "八月中秋", "風吹大地", "山林涼", "草枝擺", "🐂🍺", "🐃🍺", "八月", "中秋",
     "山林", "風吹", "大地", "草枝", "八", "月", "中", "秋", "山", "林", "涼", "風",
     "吹", "大", "地", "草", "枝", "擺", "八", "月", "中", "秋", "山", "林", "涼", "風",
     "吹", "大", "地", "草", "枝", "擺",
   ]
   static var thePool: CandidatePool {
-    let result = CandidatePool(candidates: testCandidates, rowCapacity: 6)
+    let result = CandidatePool(candidates: testCandidates, columnCapacity: 6, selectionKeys: "123456789")
     // 下一行待解決：無論這裡怎麼指定高亮選中項是哪一筆，其所在行都得被卷動到使用者眼前。
-    result.highlightHorizontal(at: 5)
+    result.highlight(at: 5)
     return result
   }
 
   static var previews: some View {
-    VwrCandidateHorizontal(controller: .init(.horizontal), thePool: thePool).fixedSize()
+    VwrCandidateVerticalBackports(controller: .init(.horizontal), thePool: thePool).fixedSize()
   }
 }
 
 @available(macOS 10.15, *)
-public struct VwrCandidateHorizontal: View {
+public struct VwrCandidateVerticalBackports: View {
   @Environment(\.colorScheme) var colorScheme
-  public var controller: CtlCandidateTDKBackports
+  public var controller: CtlCandidateTDK
   @State public var thePool: CandidatePool
   @State public var hint: String = ""
 
@@ -54,33 +52,35 @@ public struct VwrCandidateHorizontal: View {
 
   public var body: some View {
     VStack(alignment: .leading, spacing: 0) {
-      ScrollView(.vertical, showsIndicators: false) {
-        VStack(alignment: .leading, spacing: 1.6) {
-          ForEach(thePool.rangeForCurrentHorizontalPage, id: \.self) { rowIndex in
-            HStack(spacing: 10) {
-              ForEach(Array(thePool.candidateRows[rowIndex]), id: \.self) { currentCandidate in
-                currentCandidate.attributedStringForSwiftUIBackports.fixedSize()
-                  .frame(
-                    maxWidth: .infinity,
-                    alignment: .topLeading
-                  )
-                  .contentShape(Rectangle())
-                  .onTapGesture { didSelectCandidateAt(currentCandidate.index) }
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(alignment: .top, spacing: 10) {
+          ForEach(thePool.rangeForCurrentPage, id: \.self) { columnIndex in
+            VStack(alignment: .leading, spacing: 0) {
+              ForEach(Array(thePool.candidateLines[columnIndex]), id: \.self) { currentCandidate in
+                HStack(spacing: 0) {
+                  currentCandidate.attributedStringForSwiftUIBackports.fixedSize(horizontal: false, vertical: true)
+                    .frame(
+                      maxWidth: .infinity,
+                      alignment: .topLeading
+                    )
+                    .contentShape(Rectangle())
+                    .onTapGesture { didSelectCandidateAt(currentCandidate.index) }
+                }
               }
             }.frame(
-              minWidth: 0,
-              maxWidth: .infinity,
+              minWidth: Double(CandidateCellData.unifiedSize * 5),
               alignment: .topLeading
-            ).id(rowIndex)
+            ).id(columnIndex)
             Divider()
           }
-          if thePool.maximumRowsPerPage - thePool.rangeForCurrentHorizontalPage.count > 0 {
-            ForEach(thePool.rangeForLastHorizontalPageBlanked, id: \.self) { _ in
-              HStack(spacing: 0) {
-                thePool.blankCell.attributedStringForSwiftUIBackports
-                  .frame(maxWidth: .infinity, alignment: .topLeading)
-                  .contentShape(Rectangle())
-                Spacer()
+          if thePool.maxLinesPerPage - thePool.rangeForCurrentPage.count > 0 {
+            ForEach(thePool.rangeForLastPageBlanked, id: \.self) { _ in
+              VStack(alignment: .leading, spacing: 0) {
+                ForEach(0..<thePool.maxLineCapacity, id: \.self) { _ in
+                  thePool.blankCell.attributedStringForSwiftUIBackports.fixedSize()
+                    .frame(width: Double(CandidateCellData.unifiedSize * 5), alignment: .topLeading)
+                    .contentShape(Rectangle())
+                }
               }.frame(
                 minWidth: 0,
                 maxWidth: .infinity,
@@ -91,13 +91,13 @@ public struct VwrCandidateHorizontal: View {
           }
         }
       }
-      .fixedSize(horizontal: false, vertical: true).padding(5)
+      .fixedSize(horizontal: true, vertical: false).padding(5)
       .background(Color(white: colorScheme == .dark ? 0.1 : 1))
       ZStack(alignment: .leading) {
         if hint.isEmpty {
           Color(white: colorScheme == .dark ? 0.2 : 0.9)
         } else {
-          controller.highlightedColorUI
+          controller.highlightedColorUIBackports
         }
         HStack(alignment: .bottom) {
           Text(hint).font(.system(size: max(CandidateCellData.unifiedSize * 0.7, 11), weight: .bold)).lineLimit(1)
@@ -111,6 +111,5 @@ public struct VwrCandidateHorizontal: View {
         )
       }
     }
-    .frame(minWidth: thePool.maxWindowWidth, maxWidth: thePool.maxWindowWidth)
   }
 }
