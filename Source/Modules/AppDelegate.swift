929c5c5e2a0c06f1a62cccef2df4a316c8a2a6d9
ShikiSuen <shikisuen@outlook.com>
2022-10-17 01:48:33 +0800

    Repo // LMMgr.loadCassetteData() when necessary.

--- Source/Modules/AppDelegate.swift
+++ Source/Modules/AppDelegate.swift
@@ -46,6 +46,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
 
     if !PrefMgr.shared.onlyLoadFactoryLangModelsIfNeeded { LMMgr.loadDataModelsOnAppDelegate() }
     DispatchQueue.main.async {
+      LMMgr.loadCassetteData()
       LMMgr.initUserLangModels()
       self.folderMonitor.folderDidChange = { [weak self] in
         self?.reloadOnFolderChangeHappens()
