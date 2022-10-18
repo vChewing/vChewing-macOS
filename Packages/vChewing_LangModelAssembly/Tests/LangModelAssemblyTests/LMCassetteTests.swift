d6f47b62136808ebce8d9758585bb87a2f927e2c
ShikiSuen <shikisuen@outlook.com>
2022-10-18 20:49:49 +0800

    LMCassette // Partial CIN2 Support (%octagram).
    - Data loading is supported. However, the entire interaction method with Megrez needs to be reconsidered for using frequency data of words consisting of more than 1 kanji.

--- Packages/vChewing_LangModelAssembly/Tests/LangModelAssemblyTests/LMCassetteTests.swift
+++ Packages/vChewing_LangModelAssembly/Tests/LangModelAssemblyTests/LMCassetteTests.swift
@@ -19,33 +19,20 @@ private let packageRootPath = URL(fileURLWithPath: #file).pathComponents.prefix(
 private let testDataPath: String = packageRootPath + "/Tests/TestCINData/"
 
 final class LMCassetteTests: XCTestCase {
-  func testCassetteLoadWubi98() throws {
-    let pathCINFile = testDataPath + "wubi98.cin"
-    let lmCassette98 = vChewingLM.LMCassette()
-    NSLog("LMCassette: Start loading CIN.")
-    lmCassette98.open(pathCINFile)
-    NSLog("LMCassette: Finished loading CIN. Entries: \(lmCassette98.count)")
-    XCTAssertEqual(lmCassette98.charDefMap.count, 21491)
-    XCTAssertEqual(lmCassette98.keyNameMap.count, 26)
-    XCTAssertEqual(lmCassette98.nameENG, "Wubi98")
-    XCTAssertEqual(lmCassette98.nameCJK, "五笔98")
-    XCTAssertEqual(lmCassette98.maxKeyLength, 4)
-    XCTAssertEqual(lmCassette98.endKeys.count, 0)
-    XCTAssertEqual(lmCassette98.selectionKeys.count, 10)
-  }
-
   func testCassetteLoadWubi86() throws {
-    let pathCINFile = testDataPath + "wubi86.cin"
-    let lmCassette86 = vChewingLM.LMCassette()
+    let pathCINFile = testDataPath + "wubi.cin"
+    let lmCassette = vChewingLM.LMCassette()
     NSLog("LMCassette: Start loading CIN.")
-    lmCassette86.open(pathCINFile)
-    NSLog("LMCassette: Finished loading CIN. Entries: \(lmCassette86.count)")
-    XCTAssertEqual(lmCassette86.charDefMap.count, 10690)
-    XCTAssertEqual(lmCassette86.keyNameMap.count, 26)
-    XCTAssertEqual(lmCassette86.nameENG, "Wubi86")
-    XCTAssertEqual(lmCassette86.nameCJK, "五笔86")
-    XCTAssertEqual(lmCassette86.maxKeyLength, 4)
-    XCTAssertEqual(lmCassette86.endKeys.count, 0)
-    XCTAssertEqual(lmCassette86.selectionKeys.count, 10)
+    lmCassette.open(pathCINFile)
+    NSLog("LMCassette: Finished loading CIN. Entries: \(lmCassette.count)")
+    XCTAssertEqual(lmCassette.charDefMap.count, 23494)
+    XCTAssertEqual(lmCassette.keyNameMap.count, 26)
+    XCTAssertEqual(lmCassette.octagramMap.count, 14616)
+    XCTAssertEqual(lmCassette.octagramDividedMap.count, 0)
+    XCTAssertEqual(lmCassette.nameENG, "Wubi")
+    XCTAssertEqual(lmCassette.nameCJK, "五笔")
+    XCTAssertEqual(lmCassette.maxKeyLength, 4)
+    XCTAssertEqual(lmCassette.endKeys.count, 0)
+    XCTAssertEqual(lmCassette.selectionKeys.count, 10)
   }
 }
