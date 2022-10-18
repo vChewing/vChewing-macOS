292d35de3d758e0b8bed1753c601327955db9357
ShikiSuen <shikisuen@outlook.com>
2022-10-19 01:05:45 +0800

    InputHandler // Force ASCII raw output in cassette mode.

--- Source/Modules/InputHandler_HandleInput.swift
+++ Source/Modules/InputHandler_HandleInput.swift
@@ -10,6 +10,7 @@
 /// 按鍵調度模組要率先處理」的部分。據此判斷是否需要將按鍵處理委派給其它成員函式。
 
 import CocoaExtension
+import IMKUtils
 import LangModelAssembly
 import Shared
 
@@ -272,6 +273,14 @@ extension InputHandler {
       delegate.callError("Blocked data: charCode: \(input.charCode), keyCode: \(input.keyCode)")
       delegate.callError("A9BFF20E")
       delegate.switchState(state)
+      return true
+    }
+
+    // 將 Apple 動態鍵盤佈局的 RAW 輸出轉為 ABC 輸出。僅對磁帶模式啟用此步驟。
+    if IMKHelper.isDynamicBasicKeyboardLayoutEnabled, let event = input as? NSEvent,
+      prefs.cassetteEnabled, let txtABC = event.inAppleABCStaticForm.characters, !txtABC.isEmpty
+    {
+      delegate.switchState(IMEState.ofCommitting(textToCommit: txtABC))
       return true
     }
 
