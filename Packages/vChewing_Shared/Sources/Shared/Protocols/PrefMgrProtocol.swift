a264105c378e31559f20d952b1a0a5d4b63203d3
ShikiSuen <shikisuen@outlook.com>
2022-11-24 00:28:02 +0800

    PrefMgr // Add shareAlphanumericalModeStatusAcrossClients().

--- Packages/vChewing_Shared/Sources/Shared/Protocols/PrefMgrProtocol.swift
+++ Packages/vChewing_Shared/Sources/Shared/Protocols/PrefMgrProtocol.swift
@@ -53,6 +53,7 @@ public protocol PrefMgrProtocol {
   var forceCassetteChineseConversion: Int { get set }
   var showReverseLookupInCandidateUI: Bool { get set }
   var autoCompositeWithLongestPossibleCassetteKey: Bool { get set }
+  var shareAlphanumericalModeStatusAcrossClients: Bool { get set }
   var cns11643Enabled: Bool { get set }
   var cassetteEnabled: Bool { get set }
   var symbolInputEnabled: Bool { get set }
