72a7791ee2f686882295284330a44a3788fbc0ad
ShikiSuen <shikisuen@outlook.com>
2022-12-28 15:19:30 +0800

    LMReplacements // Upgrade parsing methods.

--- Packages/vChewing_LangModelAssembly/Sources/LangModelAssembly/SubLMs/lmReplacements.swift
+++ Packages/vChewing_LangModelAssembly/Sources/LangModelAssembly/SubLMs/lmReplacements.swift
@@ -1,5 +1,4 @@
 // (c) 2021 and onwards The vChewing Project (MIT-NTL License).
-// StringView Ranges extension by (c) 2022 and onwards Isaac Xen (MIT License).
 // ====================
 // This code is released under the MIT license (SPDX-License-Identifier: MIT)
 // ... with NTL restriction stating that:
@@ -51,16 +50,15 @@ extension vChewingLM {
     public mutating func replaceData(textData rawStrData: String) {
       if strData == rawStrData { return }
       strData = rawStrData
-      strData.ranges(splitBy: "\n").filter { !$0.isEmpty }.forEach {
-        let neta = strData[$0].split(separator: " ")
-        if neta.count >= 2 {
-          let theKey = String(neta[0])
-          if !neta[0].isEmpty, !neta[1].isEmpty, theKey.first != "#" {
-            let theValue = $0
-            rangeMap[theKey] = theValue
-          }
-        }
+      var newMap: [String: Range<String.Index>] = [:]
+      strData.parse(splitee: "\n") { theRange in
+        let theCells = rawStrData[theRange].split(separator: " ")
+        if theCells.count < 2 { return }
+        let theKey = theCells[0].description
+        if theKey.first != "#" { newMap[theKey] = theRange }
       }
+      rangeMap = newMap
+      newMap.removeAll()
     }
 
     public mutating func clear() {
@@ -99,20 +97,6 @@ extension vChewingLM {
 
     public func hasValuesFor(key: String) -> Bool {
       rangeMap[key] != nil
-    }
-  }
-}
-
-// MARK: - StringView Ranges Extension (by Isaac Xen)
-
-extension String {
-  fileprivate func ranges(splitBy separator: Element) -> [Range<String.Index>] {
-    var startIndex = startIndex
-    return split(separator: separator).reduce(into: []) { ranges, substring in
-      _ = range(of: substring, range: startIndex..<endIndex).map { range in
-        ranges.append(range)
-        startIndex = range.upperBound
-      }
     }
   }
 }
