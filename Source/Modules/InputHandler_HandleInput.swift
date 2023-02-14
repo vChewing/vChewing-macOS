5958ab9c2b5fd6a9af35292007979166fc73667a
ShikiSuen <shikisuen@outlook.com>
2023-02-14 17:20:19 +0800

    InputHandler // Extend Shift+Command+[] for candidate states.

--- Source/Modules/InputHandler_HandleInput.swift
+++ Source/Modules/InputHandler_HandleInput.swift
@@ -140,10 +140,8 @@ extension InputHandler {
       // 此處 JIS 鍵盤判定無法用於螢幕鍵盤。所以，螢幕鍵盤的場合，系統會依照 US 鍵盤的判定方案。
       let isJIS: Bool = KBGetLayoutType(Int16(LMGetKbdType())) == kKeyboardJIS
       switch (input.keyCode, isJIS) {
-      case (30, true): return revolveCandidate(reverseOrder: true)
-      case (42, true): return revolveCandidate(reverseOrder: false)
-      case (33, false): return revolveCandidate(reverseOrder: true)
-      case (30, false): return revolveCandidate(reverseOrder: false)
+      case (30, true), (33, false): return revolveCandidate(reverseOrder: true)
+      case (42, true), (30, false): return revolveCandidate(reverseOrder: false)
       default: break
       }
     }
