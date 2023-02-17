fd2fe6539fbc3acabea16ca9dd3f624e762adc43
ShikiSuen <shikisuen@outlook.com>
2023-02-17 13:04:03 +0800

    Prefs // +disableSegmentedThickUnderlineInMarkingMode.
    Fullname: disableSegmentedThickUnderlineInMarkingModeForManagedClients.

--- Source/Modules/PrefMgr_Core.swift
+++ Source/Modules/PrefMgr_Core.swift
@@ -182,6 +182,12 @@ public class PrefMgr: PrefMgrProtocol {
   @AppProperty(key: UserDef.kHandleDefaultCandidateFontsByLangIdentifier.rawValue, defaultValue: false)
   public var handleDefaultCandidateFontsByLangIdentifier: Bool
 
+  @AppProperty(
+    key: UserDef.kDisableSegmentedThickUnderlineInMarkingModeForManagedClients.rawValue,
+    defaultValue: false
+  )
+  public var disableSegmentedThickUnderlineInMarkingModeForManagedClients: Bool
+
   // MARK: - Settings (Tier 3)
 
   @AppProperty(key: UserDef.kMaxCandidateLength.rawValue, defaultValue: 10)
