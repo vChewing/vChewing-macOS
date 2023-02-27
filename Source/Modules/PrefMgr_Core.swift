9794d8e8e2c1c11e1d9b02ed6e2459ffe8639189
ShikiSuen <shikisuen@outlook.com>
2023-02-27 19:43:44 +0800

    Prefs // +enableMouseScrollingForTDKCandidatesCocoa.

--- Source/Modules/PrefMgr_Core.swift
+++ Source/Modules/PrefMgr_Core.swift
@@ -188,6 +188,9 @@ public class PrefMgr: PrefMgrProtocol {
   @AppProperty(key: UserDef.kEnableSwiftUIForTDKCandidates.rawValue, defaultValue: false)
   public var enableSwiftUIForTDKCandidates: Bool
 
+  @AppProperty(key: UserDef.kEnableMouseScrollingForTDKCandidatesCocoa.rawValue, defaultValue: false)
+  public var enableMouseScrollingForTDKCandidatesCocoa: Bool
+
   @AppProperty(
     key: UserDef.kDisableSegmentedThickUnderlineInMarkingModeForManagedClients.rawValue,
     defaultValue: false
