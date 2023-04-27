7d776baed58004626812f397ccd92a3dba1fb35f
ShikiSuen <shikisuen@outlook.com>
2023-04-28 01:53:58 +0800

    IMEState // Remove useless variables, etc.
    * Also reordering certain variables.

--- Packages/vChewing_Shared/Sources/Shared/Protocols/IMEStateProtocol.swift
+++ Packages/vChewing_Shared/Sources/Shared/Protocols/IMEStateProtocol.swift
@@ -21,7 +21,6 @@ public protocol IMEStateProtocol {
   var textToCommit: String { get set }
   var tooltip: String { get set }
   var tooltipDuration: Double { get set }
-  var attributedString: NSAttributedString { get }
   var convertedToInputting: IMEStateProtocol { get }
   var isFilterable: Bool { get }
   var isMarkedLengthValid: Bool { get }
@@ -53,9 +52,6 @@ public protocol IMEStateDataProtocol {
   var tooltipBackupForInputting: String { get set }
   var tooltip: String { get set }
   var tooltipDuration: Double { get set }
-  var attributedStringNormal: NSAttributedString { get }
-  var attributedStringMarking: NSAttributedString { get }
-  var attributedStringPlaceholder: NSAttributedString { get }
   var userPhraseKVPair: (keyArray: [String], value: String) { get }
   var tooltipColorState: TooltipColorState { get set }
   mutating func updateTooltipForMarking()
