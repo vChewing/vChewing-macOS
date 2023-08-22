4e0f59cd0914619b0e051da5220f8d656b228813
ShikiSuen <shikisuen@outlook.com>
2023-08-22 11:07:18 +0800

    SPM // Preparations for the next development phrase.

---
+++ Packages/vChewing_MainAssembly/Tests/MainAssemblyTests/MainAssemblyTests.swift
@@ -0,0 +1,11 @@
+@testable import MainAssembly
+import XCTest
+
+final class MainAssemblyTests: XCTestCase {
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(MainAssembly().text, "Hello, World!")
+  }
+}
