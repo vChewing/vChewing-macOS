6fcc459729b20f6ffe9ec6275eb2c3ac815f98b9
ShikiSuen <shikisuen@outlook.com>
2023-09-19 16:02:50 +0800

    Preferences // Use ObjC instead.

---
+++ Preferences/Preferences.h
@@ -0,0 +1,10 @@
+#import <PreferencePanes/PreferencePanes.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Preferences : NSPreferencePane
+@property (strong) IBOutlet NSTextField *lblDisclaimer;
+
+@end
+
+NS_ASSUME_NONNULL_END
