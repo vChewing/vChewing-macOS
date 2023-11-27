4b0ec368e78b2d795810cbe424398e2d17916bae
ShikiSuen <shikisuen@outlook.com>
2023-11-28 02:29:17 +0800

    AppInstaller // Fine-tune the visual style.

--- Installer/vChewingInstallerApp.swift
+++ Installer/vChewingInstallerApp.swift
@@ -13,12 +13,31 @@ import SwiftUI
 struct vChewingInstallerApp: App {
   var body: some Scene {
     WindowGroup {
-      MainView()
+      ZStack(alignment: .center) {
+        LinearGradient(
+          gradient: Gradient(
+            colors: [
+              Color(red: 0, green: 0, blue: 0xF4 / 255),
+              .black,
+            ]
+          ),
+          startPoint: .top, endPoint: .bottom
+        ).overlay(alignment: .topLeading) {
+          Text("vChewing Input Method")
+            .font(.system(size: 30))
+            .italic().bold()
+            .padding()
+            .foregroundStyle(Color.white)
+            .shadow(color: .black, radius: 0, x: 5, y: 5)
+        }
+        MainView()
+          .shadow(color: .black, radius: 3, x: 0, y: 0)
+      }.frame(width: 1000, height: 630)
         .onAppear {
           NSWindow.allowsAutomaticWindowTabbing = false
           NSApp.windows.forEach { window in
             window.titlebarAppearsTransparent = true
-            window.setContentSize(.init(width: 533, height: 386))
+            window.setContentSize(.init(width: 1000, height: 630))
             window.standardWindowButton(.closeButton)?.isHidden = true
             window.standardWindowButton(.miniaturizeButton)?.isHidden = true
             window.standardWindowButton(.zoomButton)?.isHidden = true
