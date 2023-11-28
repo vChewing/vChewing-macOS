668e93c330e158168d08714e79b4bbbc2c039d69
ShikiSuen <shikisuen@outlook.com>
2023-11-28 23:04:44 +0800

    AppDelegate // Set the maximum allowed memory usage to 384MB.

--- Packages/vChewing_MainAssembly/Sources/MainAssembly/AppDelegate.swift
+++ Packages/vChewing_MainAssembly/Sources/MainAssembly/AppDelegate.swift
@@ -147,7 +147,7 @@ public extension AppDelegate {
     guard let currentMemorySizeInBytes = NSApplication.memoryFootprint else { return 0 }
     let currentMemorySize: Double = (Double(currentMemorySizeInBytes) / 1024 / 1024).rounded(toPlaces: 1)
     switch currentMemorySize {
-    case 768...:
+    case 384...:
       vCLog("WARNING: EXCESSIVE MEMORY FOOTPRINT (\(currentMemorySize)MB).")
       let msgPackage = UNMutableNotificationContent()
       msgPackage.title = NSLocalizedString("vChewing", comment: "")
