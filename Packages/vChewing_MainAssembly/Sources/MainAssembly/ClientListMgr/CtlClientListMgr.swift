f8710a0173fe1cc816f4e17f619b961b4589f20e
ShikiSuen <shikisuen@outlook.com>
2024-02-08 01:33:03 +0800

    ClientListMgr // Reimplement without XIB.

---
+++ Packages/vChewing_MainAssembly/Sources/MainAssembly/ClientListMgr/CtlClientListMgr.swift
@@ -0,0 +1,62 @@
+// (c) 2021 and onwards The vChewing Project (MIT-NTL License).
+// ====================
+// This code is released under the MIT license (SPDX-License-Identifier: MIT)
+// ... with NTL restriction stating that:
+// No trademark license is granted to use the trade names, trademarks, service
+// marks, or product names of Contributor, except as required to fulfill notice
+// requirements defined in MIT License.
+
+import AppKit
+import Foundation
+
+public class CtlClientListMgr: NSWindowController {
+  let viewController = VwrClientListMgr()
+
+  public static var shared: CtlClientListMgr?
+  public init() {
+    super.init(
+      window: .init(
+        contentRect: CGRect(x: 401, y: 295, width: 770, height: 335),
+        styleMask: [.titled, .closable, .miniaturizable],
+        backing: .buffered,
+        defer: true
+      )
+    )
+    viewController.loadView()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+
+  public static func show() {
+    if shared == nil {
+      shared = CtlClientListMgr()
+    }
+    guard let shared = shared, let sharedWindow = shared.window else { return }
+    if !sharedWindow.isVisible {
+      shared.windowDidLoad()
+    }
+    sharedWindow.setPosition(vertical: .center, horizontal: .right, padding: 20)
+    sharedWindow.orderFrontRegardless() // 逼著視窗往最前方顯示
+    sharedWindow.title = "Client Manager".localized
+    sharedWindow.level = .statusBar
+    if #available(macOS 10.10, *) {
+      sharedWindow.titlebarAppearsTransparent = true
+    }
+    shared.showWindow(shared)
+    NSApp.popup()
+  }
+
+  override public func windowDidLoad() {
+    super.windowDidLoad()
+    let view = viewController.view
+    window?.contentView = view
+    if let window = window {
+      var frame = window.frame
+      frame.size = view.fittingSize
+      window.setFrame(frame, display: true)
+    }
+    window?.setPosition(vertical: .center, horizontal: .right, padding: 20)
+  }
+}
