056c932f214a444db521e6267ecdb2de2d0de9ed
ShikiSuen <shikisuen@outlook.com>
2024-02-14 03:34:19 +0800

    SettingsCocoa // Add file drag receiver button for Kimo Data import.

--- Packages/vChewing_MainAssembly/Sources/MainAssembly/Settings/SettingsCocoa/VwrSettingsPaneCocoaDictionary.swift
+++ Packages/vChewing_MainAssembly/Sources/MainAssembly/Settings/SettingsCocoa/VwrSettingsPaneCocoaDictionary.swift
@@ -8,6 +8,7 @@
 
 import AppKit
 import BookmarkManager
+import CocoaExtension
 import Foundation
 import Shared
 
@@ -16,6 +17,7 @@ public extension SettingsPanesCocoa {
     let windowWidth: CGFloat = 577
     let contentWidth: CGFloat = 512
     let pctUserDictionaryFolder: NSPathControl = .init()
+    let dragRetrieverKimo: NSFileDragRetrieverButton = .init()
 
     override public func loadView() {
       prepareUserDictionaryFolderPathControl(pctUserDictionaryFolder)
@@ -63,15 +65,25 @@ public extension SettingsPanesCocoa {
           NSStackView.build(.horizontal) {
             "i18n:settings.importFromKimoTxt.buttonText".makeNSLabel(fixWidth: contentWidth)
             NSView()
-            NSButton(
-              verbatim: "...",
-              target: self,
-              action: #selector(importYahooKeyKeyUserDictionaryData(_:))
-            )
+            importKimoDragButton()
           }
         }?.boxed()
         NSView().makeSimpleConstraint(.height, relation: .equal, value: NSFont.systemFontSize)
       }
+    }
+
+    func importKimoDragButton() -> NSFileDragRetrieverButton {
+      dragRetrieverKimo.postDragHandler = { url in
+        guard var rawString = try? String(contentsOf: url) else { return }
+        let count = LMMgr.importYahooKeyKeyUserDictionary(text: &rawString)
+        CtlSettingsCocoa.shared?.window.callAlert(
+          title: String(format: "i18n:settings.importFromKimoTxt.finishedCount:%@".localized, count.description)
+        )
+      }
+      dragRetrieverKimo.title = "i18n:kimoImportButton.DragFileToHere".localized
+      dragRetrieverKimo.target = self
+      dragRetrieverKimo.action = #selector(importYahooKeyKeyUserDictionaryData(_:))
+      return dragRetrieverKimo
     }
 
     func pathControlMainView() -> NSView? {
@@ -123,6 +135,10 @@ public extension SettingsPanesCocoa {
     }
 
     @IBAction func importYahooKeyKeyUserDictionaryData(_: NSButton) {
+      guard #available(macOS 10.13, *) else {
+        SettingsPanesCocoa.warnAboutComDlg32Inavailability()
+        return
+      }
       let dlgOpenFile = NSOpenPanel()
       dlgOpenFile.title = NSLocalizedString(
         "i18n:settings.importFromKimoTxt.buttonText", comment: ""
