8ce8bb2cdc62867577af14d1bf38c1bfd4465297
ShikiSuen <shikisuen@outlook.com>
2025-06-24 00:05:15 +0800

    LMInstantiator // Add inputTokenHashMap for future purposes.

--- Packages/vChewing_LangModelAssembly/Sources/LangModelAssembly/LMInstantiator_DateTimeExtension.swift
+++ Packages/vChewing_LangModelAssembly/Sources/LangModelAssembly/LMInstantiator_DateTimeExtension.swift
@@ -56,6 +56,10 @@ extension LMAssembly.LMInstantiator {
     }
     // 終末處理。
     let values = tokens.map { $0.parseAsInputToken(isCHS: isCHS) }.flatMap { $0 }.deduplicated
+    values.forEach { currentValue in
+      let hashKey = "\(key)\t\(currentValue)".hashValue
+      inputTokenHashMap[hashKey] = true
+    }
     var i: Double = -99
     for strValue in values.reversed() {
       results.insert(.init(value: strValue, score: i), at: 0)
