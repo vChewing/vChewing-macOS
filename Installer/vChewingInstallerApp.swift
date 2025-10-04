54cf43eaad59829bd5cb950d36af492ffefc5c78
ShikiSuen <shikisuen@outlook.com>
2025-10-04 14:30:46 +0800

    Installer // Ameliorate the background color, etc.

--- Installer/vChewingInstallerApp.swift
+++ Installer/vChewingInstallerApp.swift
@@ -13,16 +13,9 @@ import SwiftUI
 struct vChewingInstallerApp: App {
   var body: some Scene {
     WindowGroup {
-      ZStack(alignment: .center) {
-        LinearGradient(
-          gradient: Gradient(
-            colors: [
-              Color(red: 0, green: 0, blue: 0xF4 / 255),
-              .black,
-            ]
-          ),
-          startPoint: .top, endPoint: .bottom
-        ).overlay(alignment: .topLeading) {
+      makeGradient()
+        .frame(minWidth: 1_000, maxWidth: .infinity, minHeight: 630, maxHeight: .infinity)
+        .overlay(alignment: .topLeading) {
           Text("vChewing Input Method")
             .font(.system(size: 30))
             .italic().bold()
@@ -30,24 +23,25 @@ struct vChewingInstallerApp: App {
             .foregroundStyle(Color.white)
             .shadow(color: .black, radius: 0, x: 5, y: 5)
         }
-        MainView()
-          .shadow(color: .black, radius: 3, x: 0, y: 0)
-      }.frame(width: 1_000, height: 630)
-        .onAppear {
-          NSWindow.allowsAutomaticWindowTabbing = false
-          NSApp.windows.forEach { window in
-            window.titlebarAppearsTransparent = true
-            window.setContentSize(.init(width: 1_000, height: 630))
-            window.standardWindowButton(.closeButton)?.isHidden = true
-            window.standardWindowButton(.miniaturizeButton)?.isHidden = true
-            window.standardWindowButton(.zoomButton)?.isHidden = true
-            window.styleMask.remove(.resizable)
-            window.orderFront(self)
-          }
+        .overlay {
+          MainView()
+            .shadow(color: .black, radius: 3, x: 0, y: 0)
         }
-        .onDisappear {
-          NSApp.terminate(self)
+      .onAppear {
+        NSWindow.allowsAutomaticWindowTabbing = false
+        NSApp.windows.forEach { window in
+          window.titlebarAppearsTransparent = true
+          window.setContentSize(.init(width: 1_000, height: 630))
+          window.standardWindowButton(.closeButton)?.isHidden = true
+          window.standardWindowButton(.miniaturizeButton)?.isHidden = true
+          window.standardWindowButton(.zoomButton)?.isHidden = true
+          window.styleMask.remove(.resizable)
+          window.orderFront(self)
         }
+      }
+      .onDisappear {
+        NSApp.terminate(self)
+      }
     }
     .commands {
       CommandGroup(replacing: .newItem) {}
@@ -55,6 +49,36 @@ struct vChewingInstallerApp: App {
       CommandGroup(replacing: .help) {}
       CommandGroup(replacing: .appVisibility) {}
       CommandGroup(replacing: .systemServices) {}
+    }
+  }
+
+  @ViewBuilder private func makeGradient() -> some View {
+    if #available(macOS 15.0, *) {
+      MeshGradient(
+        width: 2,
+        height: 2,
+        points: [
+          [0, 0], [1, 0],
+          [0, 1], [1, 1],
+        ],
+        colors: [
+          Color(red: 28 / 255, green: 46 / 255, blue: 61 / 255),
+          Color(red: 61 / 255, green: 98 / 255, blue: 126 / 255),
+          Color(red: 145 / 255, green: 189 / 255, blue: 224 / 255),
+          Color(red: 193 / 255, green: 207 / 255, blue: 217 / 255),
+        ]
+      )
+    } else {
+      LinearGradient(
+        gradient: Gradient(
+          colors: [
+            Color(red: 28 / 255, green: 46 / 255, blue: 61 / 255),
+            Color(red: 145 / 255, green: 189 / 255, blue: 224 / 255),
+          ]
+        ),
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing
+      )
     }
   }
 }
