48bde5dea2fc79e60625eed494e3a353bc489aeb
ShikiSuen <shikisuen@outlook.com>
2025-10-10 22:07:27 +0800

    IMKUtils // Ensure compilability on Linux.

--- Packages/vChewing_IMKUtils/Sources/IMKUtils/IMKTextInputImpl.swift
+++ Packages/vChewing_IMKUtils/Sources/IMKUtils/IMKTextInputImpl.swift
@@ -6,38 +6,42 @@
 // marks, or product names of Contributor, except as required to fulfill notice
 // requirements defined in MIT License.
 
-import InputMethodKit
+#if canImport(InputMethodKit)
 
-extension Optional where Wrapped == IMKTextInput {
-  /// Constant for IMKTextOrientationName
-  /// A constant string used to reference text orientation for IMKTextInput.
-  public static var imkTextOrientationName: String { "IMKTextOrientationName" }
+  import InputMethodKit
 
-  public func updateCurrentTextInputDirection(isVertical isVerticalTyping: inout Bool) {
-    guard let client = self else {
-      isVerticalTyping = false
-      return
-    }
-    var textFrame = NSRect.zero
-    let attributes: [AnyHashable: Any]? = client.attributes(
-      forCharacterIndex: 0, lineHeightRectangle: &textFrame
-    )
-    let result = (attributes?[Self.imkTextOrientationName] as? NSNumber)?.intValue == 0 || false
-    isVerticalTyping = result
-  }
-}
+  extension Optional where Wrapped == IMKTextInput {
+    /// Constant for IMKTextOrientationName
+    /// A constant string used to reference text orientation for IMKTextInput.
+    public static var imkTextOrientationName: String { "IMKTextOrientationName" }
 
-extension IMKTextInput {
-  public func lineHeightRect(u16Cursor: Int) -> NSRect {
-    var lineHeightRect = NSRect.zero
-    var u16Cursor = u16Cursor
-    // iMessage 的話，據此算出來的 lineHeightRect 結果的橫向座標起始點不準確。目前無解。
-    while lineHeightRect.origin == .zero, u16Cursor >= 0 {
-      _ = attributes(
-        forCharacterIndex: u16Cursor, lineHeightRectangle: &lineHeightRect
+    public func updateCurrentTextInputDirection(isVertical isVerticalTyping: inout Bool) {
+      guard let client = self else {
+        isVerticalTyping = false
+        return
+      }
+      var textFrame = NSRect.zero
+      let attributes: [AnyHashable: Any]? = client.attributes(
+        forCharacterIndex: 0, lineHeightRectangle: &textFrame
       )
-      u16Cursor -= 1
+      let result = (attributes?[Self.imkTextOrientationName] as? NSNumber)?.intValue == 0 || false
+      isVerticalTyping = result
     }
-    return lineHeightRect
   }
-}
+
+  extension IMKTextInput {
+    public func lineHeightRect(u16Cursor: Int) -> NSRect {
+      var lineHeightRect = NSRect.zero
+      var u16Cursor = u16Cursor
+      // iMessage 的話，據此算出來的 lineHeightRect 結果的橫向座標起始點不準確。目前無解。
+      while lineHeightRect.origin == .zero, u16Cursor >= 0 {
+        _ = attributes(
+          forCharacterIndex: u16Cursor, lineHeightRectangle: &lineHeightRect
+        )
+        u16Cursor -= 1
+      }
+      return lineHeightRect
+    }
+  }
+
+#endif
