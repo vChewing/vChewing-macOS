65826744763c8203c3b3e7d75e085606d86e71f3
ShikiSuen <shikisuen@outlook.com>
2025-10-15 21:43:13 +0800

    OSImpl // Patch NSSound.buzz() API.

--- Packages/vChewing_OSFrameworkImpl/Sources/OSFrameworkImpl/AppKitImpl/AppKitImpl_NSSound.swift
+++ Packages/vChewing_OSFrameworkImpl/Sources/OSFrameworkImpl/AppKitImpl/AppKitImpl_NSSound.swift
@@ -13,11 +13,14 @@
 
   extension NSSound {
     public static func buzz(fart: Bool = false) {
-      let filePath = Bundle.main.path(forResource: fart ? "Fart" : "Beep", ofType: "m4a")!
+      let resName = fart ? "Fart" : "Beep"
+      let filePath = Bundle.main.path(forResource: resName, ofType: "m4a")
+      guard let filePath else { return }
       let fileURL = URL(fileURLWithPath: filePath)
       var soundID: SystemSoundID = 0
       AudioServicesCreateSystemSoundID(fileURL as CFURL, &soundID)
       AudioServicesPlaySystemSound(soundID)
+      AudioServicesDisposeSystemSoundID(soundID)
     }
 
     public static func buzz(fart: Bool = false, count: Int) {
