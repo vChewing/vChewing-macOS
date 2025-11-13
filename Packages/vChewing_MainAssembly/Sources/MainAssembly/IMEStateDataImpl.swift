b266ee8c1df83a45faf36e86baa04c6d7a09f075
ShikiSuen <shikisuen@outlook.com>
2025-11-13 13:23:33 +0800

    MainAssembly // IMEStateData: Refactor marked AttributedStrings.

--- Packages/vChewing_MainAssembly/Sources/MainAssembly/IMEStateDataImpl.swift
+++ Packages/vChewing_MainAssembly/Sources/MainAssembly/IMEStateDataImpl.swift
@@ -11,14 +11,22 @@ import AppKit
 // MARK: - AttributedString 材料生成器
 
 extension IMEStateData {
-  public var displayedTextConverted: String {
-    /// 先做繁簡轉換
-    var result = ChineseConverter.kanjiConversionIfRequired(displayedText)
-    if result.utf16.count != displayedText.utf16.count
-      || result.count != displayedText.count {
-      result = displayedText
+  /// 繁簡轉換
+  private func convertTextIfNeeded(_ rawStr: String) -> String {
+    var result = ChineseConverter.kanjiConversionIfRequired(rawStr)
+    if result.utf16.count != rawStr.utf16.count
+      || result.count != rawStr.count {
+      result = rawStr
     }
     return result
+  }
+
+  public var displayedTextConverted: String {
+    convertTextIfNeeded(displayedText)
+  }
+
+  public var displayTextSegmentsConverted: [String] {
+    displayTextSegments.map(convertTextIfNeeded)
   }
 
   // MARK: Other data for non-empty states.
@@ -45,69 +53,83 @@ extension IMEStateData {
 // MARK: - AttributedString 生成器
 
 extension IMEStateData {
-  public func attributedStringNormal(for session: IMKInputControllerProtocol)
-    -> NSAttributedString {
-    /// 考慮到因為滑鼠點擊等其它行為導致的組字區內容遞交情況，
-    /// 這裡對組字區內容也加上康熙字轉換或者 JIS 漢字轉換處理。
-    let attributedString = NSMutableAttributedString(string: displayedTextConverted)
-    var newBegin = 0
-    for (i, neta) in displayTextSegments.enumerated() {
-      let rangeNow = NSRange(location: newBegin, length: neta.utf16.count)
-      /// 不能用 .thick，否則會看不到游標。
-      var theAttributes: [NSAttributedString.Key: Any] =
-        session.mark(forStyle: kTSMHiliteConvertedText, at: rangeNow)
-          as? [NSAttributedString.Key: Any]
-          ?? [.underlineStyle: NSUnderlineStyle.single.rawValue]
-      theAttributes[.markedClauseSegment] = i
-      attributedString.setAttributes(theAttributes, range: rangeNow)
-      newBegin += neta.utf16.count
+  /// IMKInputController 的 `mark(forStyle:)` 只可能會標出這些值。
+  /// 該值乃使用 hopper disassembler 分析 IMK 而得出。
+  public enum AttrStrULStyle: Int {
+    case none = 0
+    /// #1, kTSMHiliteConvertedText & kTSMHiliteSelectedRawText
+    case single = 1
+    /// #2, kTSMHiliteSelectedConvertedText
+    case thick = 2
+    /// #3, 尚未被 TSM 使用。或可用給 kTSMHiliteSelectedRawText 與 1 區分。
+    case double = 3
+
+    // MARK: Public
+
+    public typealias StyledPair = (string: String, style: Self)
+
+    public static func pack(_ pairs: [StyledPair]) -> NSAttributedString {
+      let result = NSMutableAttributedString()
+      var clauseSegment = 0
+      for (string, style) in pairs {
+        guard !string.isEmpty else { continue }
+        result.append(style.getMarkedAttrStr(string, clauseSegment: clauseSegment))
+        clauseSegment += 1
+      }
+      return result
     }
-    return attributedString
+
+    public func getDict(clauseSegment: Int? = nil) -> [NSAttributedString.Key: Any] {
+      var result: [NSAttributedString.Key: Any] = [Self.keyName4UL: rawValue]
+      result[Self.keyName4CS] = clauseSegment
+      return result
+    }
+
+    public func getMarkedAttrStr(_ rawStr: String, clauseSegment: Int? = nil) -> NSAttributedString {
+      let result = NSMutableAttributedString(string: rawStr)
+      let rangeNow = NSRange(location: 0, length: rawStr.utf16.count)
+      result.setAttributes(getDict(clauseSegment: clauseSegment), range: rangeNow)
+      return result
+    }
+
+    // MARK: Private
+
+    private static let keyName4UL = NSAttributedString.Key(
+      rawValue: "NSUnderline"
+    )
+
+    private static let keyName4CS = NSAttributedString.Key(
+      rawValue: "NSMarkedClauseSegment"
+    )
   }
 
-  public func attributedStringMarking(for session: IMKInputControllerProtocol)
-    -> NSAttributedString {
+  public var attributedStringNormal: NSAttributedString {
     /// 考慮到因為滑鼠點擊等其它行為導致的組字區內容遞交情況，
     /// 這裡對組字區內容也加上康熙字轉換或者 JIS 漢字轉換處理。
-    let attributedString = NSMutableAttributedString(string: displayedTextConverted)
-    let u16MarkedRange = u16MarkedRange
-    let range1 = NSRange(location: 0, length: u16MarkedRange.lowerBound)
-    let range2 = NSRange(
-      location: u16MarkedRange.lowerBound,
-      length: u16MarkedRange.upperBound - u16MarkedRange.lowerBound
+    AttrStrULStyle.pack(
+      displayTextSegments.map {
+        (convertTextIfNeeded($0), .single)
+      }
     )
-    let range3 = NSRange(
-      location: u16MarkedRange.upperBound,
-      length: displayedTextConverted.utf16.count - u16MarkedRange.upperBound
-    )
-    var rawAttribute1: [NSAttributedString.Key: Any] =
-      session.mark(forStyle: kTSMHiliteConvertedText, at: range1)
-        as? [NSAttributedString.Key: Any]
-        ?? [.underlineStyle: NSUnderlineStyle.single.rawValue]
-    rawAttribute1[.markedClauseSegment] = 0
-    var rawAttribute2: [NSAttributedString.Key: Any] =
-      session.mark(forStyle: kTSMHiliteSelectedConvertedText, at: range2)
-        as? [NSAttributedString.Key: Any]
-        ?? [.underlineStyle: NSUnderlineStyle.thick.rawValue]
-    rawAttribute2[.markedClauseSegment] = 1
-    var rawAttribute3: [NSAttributedString.Key: Any] =
-      session.mark(forStyle: kTSMHiliteConvertedText, at: range3)
-        as? [NSAttributedString.Key: Any]
-        ?? [.underlineStyle: NSUnderlineStyle.single.rawValue]
-    rawAttribute3[.markedClauseSegment] = 2
-    attributedString.setAttributes(rawAttribute1, range: range1)
-    attributedString.setAttributes(rawAttribute2, range: range2)
-    attributedString.setAttributes(rawAttribute3, range: range3)
-    return attributedString
   }
 
-  public func attributedStringPlaceholder(for session: IMKInputControllerProtocol)
-    -> NSAttributedString {
-    let attributes: [NSAttributedString.Key: Any] =
-      session.mark(forStyle: kTSMHiliteSelectedRawText, at: .zero)
-        as? [NSAttributedString.Key: Any]
-        ?? [.underlineStyle: NSUnderlineStyle.single.rawValue]
-    return .init(string: "¶", attributes: attributes)
+  public var attributedStringMarking: NSAttributedString {
+    /// 考慮到因為滑鼠點擊等其它行為導致的組字區內容遞交情況，
+    /// 這裡對組字區內容也加上康熙字轉換或者 JIS 漢字轉換處理。
+    let converted = displayedTextConverted.map(\.description)
+    let range2 = markedRange
+    let range1 = 0 ..< markedRange.lowerBound
+    let range3 = markedRange.upperBound ..< converted.count
+    let pairs: [AttrStrULStyle.StyledPair] = [
+      (converted[range1].joined(), .single),
+      (converted[range2].joined(), .thick),
+      (converted[range3].joined(), .single),
+    ]
+    return AttrStrULStyle.pack(pairs)
+  }
+
+  public var attributedStringPlaceholder: NSAttributedString {
+    AttrStrULStyle.single.getMarkedAttrStr("¶", clauseSegment: 0)
   }
 }
 
