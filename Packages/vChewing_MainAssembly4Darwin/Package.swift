08670f4390ec4148405033b729c1c3f2ddacfbd9
ShikiSuen <shikisuen@outlook.com>
2026-09-18 04:34:51 +0800

    DictionaryData - 20260918

--- Packages/vChewing_MainAssembly4Darwin/Package.swift
+++ Packages/vChewing_MainAssembly4Darwin/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
     .package(path: "../vChewing_TooltipUI"),
     .package(path: "../vChewing_Uninstaller"),
     .package(path: "../vChewing_UpdateSputnik"),
-    .package(url: "https://atomgit.com/vChewing/vChewing-VanguardLexicon.git", exact: "4.8.0"),
+    .package(url: "https://atomgit.com/vChewing/vChewing-VanguardLexicon.git", exact: "4.8.1"),
   ],
   targets: [
     .target(
